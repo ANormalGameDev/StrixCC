a4d24e1f7ff2a812b39257a7f954d94b9e26d174
ANormalGameDev <anormalgamedev@gmail.com>
2022-05-04 15:01:14 +0530

    Updated Moves.cc

--- src/Chess/Moves.cc
+++ src/Chess/Moves.cc
@@ -8,6 +8,5 @@ uint64 MoveGenerator::RookAttacks(Square square, uint64 occupancy){
 
 uint64 MoveGenerator::BishopAttacks(Square square, uint64 occupancy){
     Magic& magic = BMagics[(int)square];
-    Utils::PrintBitboard(occupancy & magic.Mask);
     return magic.AttackTable[magic.GetIndexFromOccupancy(occupancy)];
 }
