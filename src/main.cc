b7ff4329bc980536b6a74b075e2080c27dc2ee46
ANormalGameDev <anormalgamedev@gmail.com>
2022-05-04 14:59:11 +0530

    Updated main.cc (Minor Bugs remaining)

--- src/main.cc
+++ src/main.cc
@@ -6,12 +6,12 @@ int main(){
     controller.Initialize();
     
     // For Testing Purposes
-    std::cout << "Getting Requested Attack\n";
-    uint64 TestOccupancy = 0ULL;
-    Utils::SetBit(&TestOccupancy, Square::C5);
-    Utils::SetBit(&TestOccupancy, Square::E8);
-    Utils::SetBit(&TestOccupancy, Square::F5);
-    Utils::PrintBitboard(TestOccupancy);
-    uint64 att = controller.GetMoveGenerator().BishopAttacks((Square)40, TestOccupancy);
-    Utils::PrintBitboard(att);
+    // std::cout << "Getting Requested Attack\n";
+    // uint64 TestOccupancy = 0ULL;
+    // Utils::SetBit(&TestOccupancy, Square::C5);
+    // Utils::SetBit(&TestOccupancy, Square::E8);
+    // Utils::SetBit(&TestOccupancy, Square::F5);
+    // Utils::PrintBitboard(TestOccupancy);
+    // uint64 att = controller.GetMoveGenerator().BishopAttacks(Square::D7, TestOccupancy);
+    // Utils::PrintBitboard(att);
 }
